ba66862e84dc5611a23614a73427d091de641963
Zoltan Kovacs <157127975+ZoltanSmith@users.noreply.github.com>
2026-07-02 17:42:09 +0200

    képek javítása

--- MAUI_NovenyHatarozo/ViewModel/NovenyListVM.cs
+++ MAUI_NovenyHatarozo/ViewModel/NovenyListVM.cs
@@ -1,9 +1,6 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using MAUI_NovenyHatarozo.Model;
-using MAUI_NovenyHatarozo.Pages;
-using Microsoft.Maui.Controls;
 using System.Collections.ObjectModel;
-using System.Windows.Input;
 
 namespace MAUI_NovenyHatarozo.ViewModel
 {
@@ -21,17 +18,17 @@ namespace MAUI_NovenyHatarozo.ViewModel
         {
             Novenyek.Add(new Noveny() {
                 Nev = "Tulipán", Vizigeny = 1.5,
-                Kep = "https://hu.wikipedia.org/wiki/F%C3%A1jl:Tulipan_(Ama).JPG",
+                Kep = "https://t4.ftcdn.net/jpg/00/62/01/43/240_F_62014393_uS0FCMLRmds2nGWkYVVGNE2n3w5VAC6u.jpg",
                 Orokzold = false });
             Novenyek.Add(new Noveny() {
                 Nev = "Fenyő",
                 Vizigeny = 0.5,
-                Kep = "https://karacsonyfa-vasar.hu/wp-content/uploads/2016/11/dreamstimeextralarge_21507949-2.jpg",
+                Kep = "https://t4.ftcdn.net/jpg/16/11/65/57/240_F_1611655705_fsoKBQHKXCPFOT2QfblDyG7nqhqrTjux.jpg",
                 Orokzold = true });
             Novenyek.Add(new Noveny() {
                 Nev = "Paradicsom", 
                 Vizigeny = 2.0,
-                Kep = "https://upload.wikimedia.org/wikipedia/commons/thumb/8/89/Tomato_je.jpg/250px-Tomato_je.jpg", 
+                Kep = "https://t4.ftcdn.net/jpg/01/90/92/07/240_F_190920722_OA6eLB4LWmTxyuayRqa4V2jFGVbXIssX.jpg", 
                 Orokzold = false });
         }
 
